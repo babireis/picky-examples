04b2bebaa6707818e720fdb4d5fa48871325c481
Gabriele Di Stefano <gabriele.ds@gmail.com>
2015-03-14 00:00:32 +0100

    working ui, adding bower

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,17 +4,30 @@ module.exports = function (grunt) {
   grunt.initConfig({
 
     'picky': {
-      options: {
-        reporter: 'spec'
+
+      'test': {
+        options: {
+          reporter: 'spec'
+        },
+        src: 'test/**/*.@(test|feature|library|dictionay|steps)?(.js)'
       },
-      src: './test/**/*@(\.feature|\.test\.js)'
+
+      'ui': {
+        options: {
+          ui: true,
+          reporter: 'spec'
+        },
+        src: 'test/**/*.@(test|feature|library|dictionay|steps)?(.js)'
+      }
+
     }
 
   });
 
   grunt.loadNpmTasks('picky');
 
-  grunt.registerTask('test', ['picky']);
+  grunt.registerTask('test', ['picky:test']);
+  grunt.registerTask('test:ui', ['picky:test:ui']);
   grunt.registerTask('default', ['test']);
 
 }
